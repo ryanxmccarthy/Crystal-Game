cc83050c37eb9bcd3eacfbfb5d64ed3bc9eb905f
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-09-17 12:19:00 -0400

    added more crystal images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ var targetNumber = randomNumber();
 var wins = 0;
 var losses = 0;
 var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var crystalImages = ["http://images.clipartpanda.com/crystal-clipart-Crystals-occur-in-nature-2016012906.svg", 'https://www.bestcrystals.com/Content/Images/uploaded/NewFolder2/KyaniteImage.jpg', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Sodalith_-_Rohstein.jpg/766px-Sodalith_-_Rohstein.jpg', 'http://24.media.tumblr.com/56d2cea9a56966fa3e1d26d559579c6b/tumblr_mv0wya4Kev1rmdrjqo1_400.png', 'http://www.billboard.com/files/media/05-crystals-billboard-2017-embed.jpg']
 
 function addWin() {
   $('#wins').text(wins);
@@ -16,7 +17,7 @@ function makeCrystals() {
   for (var i = 0; i < 4; i++) {
     var imageCrystal = $("<img>");
     imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    imageCrystal.attr("src", crystalImages[Math.floor(Math.random() * crystalImages.length)]);
     imageCrystal.attr("data-crystalvalue", numberOptions[Math.floor(Math.random() * numberOptions.length)]);
     $("#crystals").append(imageCrystal);
   }
